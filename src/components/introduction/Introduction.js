57f0add4aaf8980c25ccdb35fa62967a30634611
Evie Polden <admin@bishopandpolden.com>
2019-07-22 21:18:55 +0100

    Maintenance message

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -2,14 +2,28 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledIntroduction = styled.div`
-  display: flex;
-  justify-content: center;
+  display: grid;
+  grid-template-areas: 'warning' 'img';
+  grid-template-rows: 5% 95%;
+  height: 100%;
+  align-self: center;
+
+  & .warning {
+    grid-area: warning;
+    text-align: center;
+  }
+
+  & .img {
+    grid-area: img;
+    justify-self: center;
+  }
 `;
 
 const Introduction = () => {
   return (
     <StyledIntroduction className="intro">
-      <img src={process.env.PUBLIC_URL + '/frontpage.png'} />
+      <p  className="warning">Down for maintenance</p>
+      <img src={process.env.PUBLIC_URL + '/frontpage.png'} className="img"/>
     </StyledIntroduction>
   );
 };
